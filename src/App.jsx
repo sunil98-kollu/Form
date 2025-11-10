582517daf0c237661c2a4c2eb87dbaa934ff9701
sunil kollu <sunil98-kollu>
2025-11-10 19:25:17 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
 export default function App() {
- 
+ const[data,setdata]=React.useState({name:'',age:''});
+
+function handleChange(e){
+ setdata({
+      ...data,
+      [e.target.name]: e.target.value
+    });
+}
+   function onsubmit(e){
+    e.preventDefault();
+        console.log(data);
+   }
   return (
     <>
       <h1>react form</h1>
+     <>
     
+    <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          value={data.name}
+          onChange={handleChange}
+          required
+        /><br/><br/>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          value={data.email}
+          onChange={handleChange}
+          required
+        /><br/><br/>
+      <button onClick={onsubmit}>submit</button>
+    </>
     </>
   )
 }
